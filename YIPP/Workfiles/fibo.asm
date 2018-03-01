25ace8910b62604750c641ebffffddb2ad92bd9b
JakeSmokie <jakesmokie@gmail.com>
2018-03-01 19:01:12 +0300

    fixed listing

--- YIPP/Workfiles/fibo.asm
+++ YIPP/Workfiles/fibo.asm
@@ -1,13 +1,43 @@
+	times 0x10 nop
 	[section _code, Code]
-	nop
-	push 0xFF
-	push *N
-	pop
-	pop
+entry:
+	jmp fibo
 
+; FUNCTIONS
+func_dec_i:
+	push *i
+	dec
+	save *i 	; i--
+
+	ret
+
+fibo:
+	read
+	save *i 	; i = N
+loop:
+	push *a
+	push *b
+
+	; analog of for (i = N; i > 0; i--)
+	
+	add 		; top = a + b
+	
+	push *a
+	save *b 	; b = a
+
+	save *a 	; a = top
+
+	call func_dec_i, *i
+
+	push *i
+	jnz loop
+result:
+	push *a
+	print   	; print a
+	
 	[section _data, Data]
 N:	dd 0x10 		; N-th number of fibonacci
 i:	dd 0x0 		 
 a:	dd 0x0		
 b:	dd 0x1
-arr: times 0x10 dd 0xFF
+	
